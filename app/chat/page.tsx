e459a51317d03349f2204da7c242802230f3dc8d
Amonion <amonionprime@gmail.com>
2025-10-26 15:05:49 +0100

    update chat fast loading

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -309,7 +309,7 @@ const Chats = () => {
     <>
       {/* <div className="fixed bottom-[55px] sm:bottom-0 sm:pt-[0px] pt-[120px] inset-0 sm:relative sm:h-[85vh] sm:rounded-[10px] bg-[var(--secondary)] flex flex-col"> */}
       {username ? (
-        <div className="flex-1 sm:relative w-full overflow-auto   sm:pb-1 flex flex-col">
+        <div className="flex-1 sm:relative w-full sm:h-[100vh] sm:overflow-hidden overflow-auto   sm:pb-1 flex flex-col">
           <div className="sticky z-30 left-0 py-2 top-0 w-full bg-[var(--primary)] mb-2 h-[65px]">
             <ChatHead />
           </div>
