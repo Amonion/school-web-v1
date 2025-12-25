fbc1a4f7a0253405d19df1b585c8a27d51cad8dc
Amonion <amonionprime@gmail.com>
2025-12-25 01:44:23 +0100

    Update exams

--- app/home/questions/page.tsx
+++ app/home/questions/page.tsx
@@ -1,30 +1,30 @@
 'use client'
-// import { useEffect, useRef } from 'react'
-// import ExamStore from '@/src/zustand/exam/Exam'
+import { useEffect, useRef } from 'react'
+import ExamStore from '@/src/zustand/exam/Exam'
 // import QuestionCard from '@/components/Home/Exam/QuestionResources/QuestionCard'
 
 export default function QuestionList() {
-  // const { hasMore, loading, exams, getMoreSavedExams } = ExamStore()
-  // const lastCardRef = useRef<HTMLDivElement | null>(null)
+  const { hasMore, loading, exams, getMoreSavedExams } = ExamStore()
+  const lastCardRef = useRef<HTMLDivElement | null>(null)
 
-  // useEffect(() => {
-  //   if (!lastCardRef.current) return
+  useEffect(() => {
+    if (!lastCardRef.current) return
 
-  //   const observer = new IntersectionObserver(
-  //     (entries) => {
-  //       if (entries[0].isIntersecting && hasMore && !loading) {
-  //         getMoreSavedExams()
-  //       }
-  //     },
-  //     { threshold: 0.5 }
-  //   )
-  //   observer.observe(lastCardRef.current)
-  //   return () => observer.disconnect()
-  // }, [exams.length])
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && hasMore && !loading) {
+          getMoreSavedExams()
+        }
+      },
+      { threshold: 0.5 }
+    )
+    observer.observe(lastCardRef.current)
+    return () => observer.disconnect()
+  }, [exams.length])
 
   return (
     <div className="flex flex-col pb-[55px] sm:pb-0 w-full">
-      {true && (
+      {loading && (
         <div className="flex items-center h-10 justify-center flex-wrap w-full">
           <i
             className={`bi  bi-opencollective loading  text-md text-[var(--custom-color)]`}
